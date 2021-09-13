24c4b3676974f22dcde65f6dcf480278bf80ca1d
Yraida Guillén Paredes <yguillenparedes@gmail.com>
2021-09-13 19:23:14 -0400

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "../src/styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var valorQuien = who[Math.floor(Math.random() * (who.length - 0) + 0)];
+  var valorQue = what[Math.floor(Math.random() * (what.length - 0) + 0)];
+  var valorCuando = when[Math.floor(Math.random() * (when.length - 0) + 0)]
+
+  excusa = `${valorQuien} ${valorQue} ${valorCuando}`;
+  document.getElementById("excuse").innerHTML = excusa;
+  console.log(excusa);
 };
